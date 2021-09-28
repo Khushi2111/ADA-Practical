19f7f7a91ce17119bc952b73dcb537d5bc08ccdb
Khushi2111 <82765191+Khushi2111@users.noreply.github.com>
2021-09-28 15:45:23 +0530

    Add files via upload

---
+++ NaiveSearch.java
@@ -0,0 +1,24 @@
+
+public class NaiveSearch {
+
+	public static void search(String txt, String pat){
+		int M = pat.length();
+		int N = txt.length();
+
+		for (int i = 0; i <= N - M; i++){
+
+			int j;
+			for (j = 0; j < M; j++)
+				if (txt.charAt(i + j) != pat.charAt(j))
+					break;
+
+			if (j == M) 
+				System.out.println("Pattern found at index " + i);
+		}
+	}
+	public static void main(String[] args){
+		String txt = "ABAACAADAABAABA";
+		String pat = "AACA";
+		search(txt, pat);
+	}
+}
